c71b3cadd6ef6e4f528c5d554fbea348a42c43b6
Sebastien La Duca <sladuca777@gmail.com>
2021-03-25 14:13:52 -0400

    finish camera output

--- src/models.cpp
+++ src/models.cpp
@@ -1,15 +1,5 @@
-#include <memory>
-#include<opencv2/opencv.hpp>
-typedef struct FingerOutput {
-    int x;
-    int y;
-    int idx;
-} FingerOutput;
+#include <whiteboard_pal/main.hpp>
 
-typedef struct GestureOutput {
-    bool gesture;
-    int idx;
-} GestureOutput;
 
 extern "C" {
     FingerOutput finger_tracking(void *frame, int idx);
@@ -30,9 +20,4 @@ GestureOutput gesture_detection(void *frame, int idx) {
         false,
         idx
     };
-}
-
-size_t write_output(void *frame, int fd, size_t size) {
-    cv::Mat *mat = static_cast<cv::Mat*>(frame);
-    return (size_t) write(fd, frame->data, size);
 }
