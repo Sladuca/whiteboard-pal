0d7f846a2b511f8b1f2502b4df95b49bfdc126b9
ZacchaeusWilliams <zvw@andrew.cmu.edu>
2021-04-21 10:15:31 -0400

    Debugging Fingertracking code

--- src/main.cpp
+++ src/main.cpp
@@ -182,7 +182,7 @@ int gesture(frame_chan_t &to_gesture, gesture_chan_t &from_gesture) {
 
 int finger(frame_chan_t &to_finger, finger_chan_t &from_finger) {
     frame_with_idx_t frame;
-    deque <point_t> points;
+    deque <Point> points;
     while (boost::fibers::channel_op_status::success == to_finger.pop(frame)) {
         frame.perf->finger_start = chrono::steady_clock::now();
         finger_output_t f = finger_tracking(frame.frame, frame.i, &points);
