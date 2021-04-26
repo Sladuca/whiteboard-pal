bd7682e826b9fa84db3b18ec4c29b8723b0eb9bb
jennyyu73 <jennyyu73@gmail.com>
2021-04-26 13:45:32 -0400

    added gesture calculator code

--- mediapipe/graphs/whiteboard_pal/calculators/gesture.cc
+++ mediapipe/graphs/whiteboard_pal/calculators/gesture.cc
@@ -6,6 +6,8 @@
 #include "mediapipe/framework/port/ret_check.h"
 #include "mediapipe/framework/port/status.h"
 #include "mediapipe/framework/formats/landmark.pb.h"
+#include <algorithm> //for min function
+#include <cmath> //for abs function
 
 namespace mediapipe {
 
@@ -20,7 +22,7 @@ namespace mediapipe {
     constexpr bool stub_has_gesture = true;
 
     class WhiteboardPalGestureDetectionCalculator: public CalculatorBase {
-        public: 
+        public:
             static absl::Status GetContract(CalculatorContract* cc) {
                 // set types for landmark inputs
                 cc->Inputs().Tag(LANDMARKS_TAG).Set<std::vector<NormalizedLandmarkList, std::allocator<NormalizedLandmarkList>>>();
@@ -39,6 +41,34 @@ namespace mediapipe {
 
             // ! expects the substrate mat to be in RGB format
             absl::Status Process(CalculatorContext* cc) {
+                const auto &landmark_vec = cc->Inputs().Tag(LANDMARKS_TAG).Get<std::vector<NormalizedLandmarkList,
+                                                                        std::allocator<NormalizedLandmarkList>>>();
+                const auto landmark_list = landmark_vec[0];
+                LOG(INFO) << "got landmark list" << "\n";
+
+                //get finger coordinates
+                //index finger
+                float index_x = landmark_list.landmark(8).x();
+                float index_y = landmark_list.landmark(8).y();
+                float index_dip = landmark_list.landmark(7).y();
+                //thumb
+                float thumb_x = landmark_list.landmark(4).x();
+                float thumb_y = landmark_list.landmark(4).y();
+                //middle finger
+                float middle_x = landmark_list.landmark(12).x();
+                float middle_y = landmark_list.landmark(12).y();
+                float knuckle_x = landmark_list.landmark(9).x();
+                float knuckle_y = landmark_list.landmark(9).y();
+                //ring and pinky
+                float ring_y = landmark_list.landmark(16).y();
+                float pinky_y = landmark_list.landmark(20).y();
+
+                float key_x = std::min(knuckle_x, middle_x);
+
+                bool clenched = (pinky_y > knuckle_y) && (middle_y > knuckle_y) && (pinky_y > knuckle_y);
+                stub_has_gesture = (std::abs(key_x - thumb_x) > std::abs(index_y - knuckle_y)/1.7f) && \
+                  clenched && (index_dip > index_y);
+
                 cc->Outputs().Tag(COORDS_TAG).Add(&stub_coords, cc->InputTimestamp());
                 cc->Outputs().Tag(HAS_GESTURE_TAG).Add(&stub_has_gesture, cc->InputTimestamp());
                 return absl::OkStatus();
