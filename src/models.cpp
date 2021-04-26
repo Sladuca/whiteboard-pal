fd79b4ab35e67fffdc99be8a897fe8f8d77a2a95
ZacchaeusWilliams <zvw@andrew.cmu.edu>
2021-04-26 10:52:33 -0400

    Getting a conv2d error from tensor flow

--- src/models.cpp
+++ src/models.cpp
@@ -52,91 +52,91 @@ finger_output_t finger_tracking(Mat frame, int idx, deque<Point> &points) {
     std::vector<std::vector<cv::Point> > contours;
     inRange(hsv, Scalar(60, 90, 50), Scalar(140, 255, 255), hsv_range); // Used Jenny's filtering constants
     // These were just 2d arrays of ones before
-    erosion_ele    = getStructuringElement(0/*Morph Rect*/,
-		                           Size(2*EROSION_SIZE+1,2*EROSION_SIZE+1),
-					   Point(EROSION_SIZE,EROSION_SIZE)); //Default point maybe should be 5,5
-    morph_ele      = getStructuringElement(0/*Morph Rect*/,
-		                           Size(2*MORPH_SIZE+1,2*MORPH_SIZE+1),
-					   Point(MORPH_SIZE,MORPH_SIZE));
-    dialation_ele  = getStructuringElement(0/*Morph Rect*/,
-		                           Size(2*DIALATION_SIZE+1,2*DIALATION_SIZE+1),
-					   Point(DIALATION_SIZE,DIALATION_SIZE));
-    printf("Second\n");
+    //erosion_ele    = getStructuringElement(0/*Morph Rect*/,
+//		                           Size(2*EROSION_SIZE+1,2*EROSION_SIZE+1),
+//					   Point(EROSION_SIZE,EROSION_SIZE)); //Default point maybe should be 5,5
+  //  morph_ele      = getStructuringElement(0/*Morph Rect*/,
+//		                           Size(2*MORPH_SIZE+1,2*MORPH_SIZE+1),
+//					   Point(MORPH_SIZE,MORPH_SIZE));
+  //  dialation_ele  = getStructuringElement(0/*Morph Rect*/,
+//		                           Size(2*DIALATION_SIZE+1,2*DIALATION_SIZE+1),
+//					   Point(DIALATION_SIZE,DIALATION_SIZE));
+  //  printf("Second\n");
     
-    imshow("1: HSV_Image", hsv);
-    erode(hsv,erosion,erosion_ele);
-    imshow("2: Erpsion", erosion);
-    morphologyEx(erosion, morph, 2 /*MORPH_OPEN*/, morph_ele);
-    imshow("3: Morph Cleanup", morph);
-    dilate(morph, dialation,dialation_ele);
-    imshow("4: Dialation", dialation);
+    //imshow("1: HSV_Image", hsv);
+//    erode(hsv,erosion,erosion_ele);
+    //imshow("2: Erpsion", erosion);
+ //   morphologyEx(erosion, morph, 2 /*MORPH_OPEN*/, morph_ele);
+    //imshow("3: Morph Cleanup", morph);
+  //  dilate(morph, dialation,dialation_ele);
+    //imshow("4: Dialation", dialation);
     //Dialation mask should be the noiseless binary output of hand position
     
-    string ty =  type2str( dialation.type() );
-    printf("Matrix: %s %dx%d \n", ty.c_str(), dialation.cols, dialation.rows );
+  //  string ty =  type2str( dialation.type() );
+   // printf("Matrix: %s %dx%d \n", ty.c_str(), dialation.cols, dialation.rows );
     
-    printf("Third\n");
+   // printf("Third\n");
     // These two defines are located in cv::traits
     //cvStartFindContours_Impl(frame, contours, 0/*CV_RETR_EXTERNAL*/, 2/*CV_CHAIN_APPROX_SIMPLE*/);
     //cvtColor(dialation, gray, Imgproc.CV_BGR2GRAY);
-    printf("This is the type of matrix it is %s \n", CV_MAT_TYPE(dialation));
+   // printf("This is the type of matrix it is %s \n", CV_MAT_TYPE(dialation));
     //if(CV_MAT_TYPE(dialation) == CV_32SC1) printf("Radical Re\n\n");
    
-    Mat hsv_channels[3];
-    split(dialation,hsv_channels);
+    //Mat hsv_channels[3];
+   // split(dialation,hsv_channels);
     
-    findContours(hsv_channels[2], contours, 0 /*CV_RETR_EXTERNAL*/, 2 /*CV_CHAIN_APPROX_SIMPLE*/);
-    vector<Point> max_cont;
-    double max_cont_area = 0;
-    int max_cont_idx     = 0;
+   // findContours(hsv_channels[2], contours, 0 /*CV_RETR_EXTERNAL*/, 2 /*CV_CHAIN_APPROX_SIMPLE*/);
+   // vector<Point> max_cont;
+   // double max_cont_area = 0;
+   // int max_cont_idx     = 0;
 
-    for(int i = 0; i< contours.size(); i++){
+   // for(int i = 0; i< contours.size(); i++){
 
-        double area = fabs(contourArea(contours[i]));
-	if( area > max_cont_area){
-          max_cont_area = area;
-	  max_cont      = contours[i];
-	  max_cont_idx  = i;
-      }
-    }
-    printf("Contour List Size: %ld, Index of Maximum Area Contour: %d\n",contours.size(), max_cont_idx);
-    print_point_vec(ref(contours[max_cont_idx]));
+   //     double area = fabs(contourArea(contours[i]));
+//	if( area > max_cont_area){
+//          max_cont_area = area;
+//	  max_cont      = contours[i];
+//	  max_cont_idx  = i;
+  //    }
+   // }
+    //printf("Contour List Size: %ld, Index of Maximum Area Contour: %d\n",contours.size(), max_cont_idx);
+    //print_point_vec(ref(contours[max_cont_idx]));
     //Farthest point
     //New way to find max centroid
-    printf("Fourth\n");
+    //printf("Fourth\n");
     
-    Moments m = moments(max_cont, true);
-    cv::Point farthest_point;
-    Point centroid(m.m10/m.m00, m.m01/m.m00);
+//    Moments m = moments(max_cont, true);
+ //   cv::Point farthest_point;
+ //   Point centroid(m.m10/m.m00, m.m01/m.m00);
     
     
-    Mat contour_mat(frame.size(), CV_8UC3, Scalar(0,0,0));
-    Scalar colors[3];
-    colors[0] = Scalar(255,0,0);
-    colors[1] = Scalar(0,255,0);
-    colors[2] = Scalar(0,0,255);
-    drawContours(contour_mat,contours, max_cont_idx,colors[0]);
+ //   Mat contour_mat(frame.size(), CV_8UC3, Scalar(0,0,0));
+ //   Scalar colors[3];
+  //  colors[0] = Scalar(255,0,0);
+ //   colors[1] = Scalar(0,255,0);
+ //   colors[2] = Scalar(0,0,255);
+//    drawContours(contour_mat,contours, max_cont_idx,colors[0]);
     
 
-    printf("Fifth\n");
-    imshow("5: Contour Display", contour_mat);
+    //printf("Fifth\n");
+    //imshow("5: Contour Display", contour_mat);
     //Slow method
-    int max_dist = 0;
-    int max_idx  = 0;
-    for(int i = 0; i< max_cont.size(); i++){
-      int tmp1 = (max_cont[i].x - centroid.x);
-      int tmp2 = (max_cont[i].y - centroid.y);
+//    int max_dist = 0;
+ //   int max_idx  = 0;
+ //   for(int i = 0; i< max_cont.size(); i++){
+  //    int tmp1 = (max_cont[i].x - centroid.x);
+   //   int tmp2 = (max_cont[i].y - centroid.y);
 
-      int tmp = tmp1 * tmp1 + tmp2 * tmp2;
-      if(tmp > max_dist){
-        max_idx = i;
-	max_dist = tmp;
-      }
-    }
-    printf("Sixth\n");
-    print_point_vec(ref(max_cont));
-    points.push_back(max_cont[max_idx]);
-    printf("Seventh\n");
+   //   int tmp = tmp1 * tmp1 + tmp2 * tmp2;
+  //    if(tmp > max_dist){
+ //       max_idx = i;
+//	max_dist = tmp;
+  //    }
+ //   }
+    //printf("Sixth\n");
+    //print_point_vec(ref(max_cont));
+//    points.push_back(max_cont[max_idx]);
+    //printf("Seventh\n");
     return finger_output_t {
         200,
         200,
